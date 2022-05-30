c25c590561c5ca2809df5a4f1159e2d0fcd9e091
kr1ps <k.rodriguez.mejia@gmail.com>
2022-05-30 21:03:56 +0200

    update repo
    Signed-off-by: kr1ps <k.rodriguez.mejia@gmail.com>

--- build-ubuntu-2204.sh
+++ build-ubuntu-2204.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # variable files ending with .auto.pkrvars.hcl are automatically loaded
-PACKER_LOG=1 PACKER_LOG_PATH=packer.log packer build -var-file="credentials.pkr.hcl" ./ubuntu-server-jammy/ubuntu-server-jammy.pkr.hcl
+PACKER_LOG=1 PACKER_LOG_PATH=packer.log packer build -var-file="../credentials.pkr.hcl" .
+#PACKER_LOG=1 PACKER_LOG_PATH=packer.log packer build -var-file="credentials.pkr.hcl" ./ubuntu-server-jammy/ubuntu-server-jammy.pkr.hcl
